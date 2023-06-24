81bc93f1d1390fd70d146f784c68a2dfdf33ba85
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-06-24 11:48:18 +0300

    Added FundMe

---
+++ 10.Libraries/FundMe.sol
@@ -0,0 +1,22 @@
+//  SPDX-License-Identifier: MIT
+pragma solidity 0.8.18; 
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+
+    function fund() public payable{
+        require(msg.value.getConversionRate() >= 1e18, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000 -> 1 eth
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    // function withdraw(){}
+}
