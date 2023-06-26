15b7809c4edff44983584b6154309ca22d92aed7
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-06-26 08:44:22 +0300

    Added PriceConverter

---
+++ 11.BasicForLoop/PriceConverter.sol
@@ -0,0 +1,28 @@
+//  SPDX-License-Identifier: MIT
+pragma solidity 0.8.18; 
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+
+    function getPrice() internal view returns(uint256){
+       // ABI
+       // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+       AggregatorV3Interface priceFeed =  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+       (,int price,,,) = priceFeed.latestRoundData();
+       // ETH in terms of USD
+      return uint256(price * 1e10);
+    }
+
+    function getVersion() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed =  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        // Always multiple before you divide
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
