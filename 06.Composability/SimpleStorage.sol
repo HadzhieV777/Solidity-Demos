05707d613817bc7e237e10acd533993aa818769f
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-07-01 08:30:25 +0300

    Fixing version

--- 06.Composability/SimpleStorage.sol
+++ 06.Composability/SimpleStorage.sol
@@ -1,15 +1,12 @@
 //  SPDX-License-Identifier: MIT
-pragma solidity 0.8.18; 
+pragma solidity 0.8.19;
 
 contract SimpleStorage {
     uint256 favoriteNumber;
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    People public person = People({
-         favoriteNumber: 2, 
-         name: "Pesho"
-    });
+    People public person = People({favoriteNumber: 2, name: "Pesho"});
 
     /*
        Basic Mappings
@@ -17,19 +14,18 @@ contract SimpleStorage {
     */
 
     struct People {
-      uint256 favoriteNumber;
-      string name;
+        uint256 favoriteNumber;
+        string name;
     }
 
     People[] public people;
 
-   
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
 
-    function addPerson (string memory _name, uint256 _favoriteNumber) public {
-      people.push(People(_favoriteNumber, _name));
-      nameToFavoriteNumber[_name] = _favoriteNumber; // Add to mapping
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber; // Add to mapping
     }
 }
