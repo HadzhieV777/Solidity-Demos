8500ef156175b9a9b0ef85734be4bb8926f05582
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-07-05 08:25:00 +0300

    Added FundMe

---
+++ 16.AdvancedCustomErrors/FundMe.sol
@@ -0,0 +1,55 @@
+//  SPDX-License-Identifier: MIT
+pragma solidity 0.8.18; 
+
+import "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor(){
+        i_owner = msg.sender; 
+        
+    }
+
+    function fund() public payable{
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough!"); 
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner{
+       
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+     
+        funders = new address[](0);
+   
+        payable(msg.sender).transfer(address(this).balance);
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed!");
+        
+       (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+       require(callSuccess, "Call failed!");
+    }
+
+    modifier onlyOwner {
+        /*
+            we can optimize the contract by editing the error log, because now each character
+            is stored individually 
+        */
+        // require(msg.sender == i_owner, "Sender is not owner!");
+        if(msg.sender != i_owner) { revert NotOwner(); }
+        _; 
+    }
+}
