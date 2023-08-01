50a6f14d9cc1e924fd97fc47945ebe19134931be
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-08-01 08:49:23 +0300

    Edited hardhat.config

--- hardhat-smartcontract-lottery-fcc/hardhat.config.js
+++ hardhat-smartcontract-lottery-fcc/hardhat.config.js
@@ -1,12 +1,33 @@
 require("@nomiclabs/hardhat-waffle")
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
-};
+    defaultNetwork: "hardhat",
+    networks: {
+      hardhat: {
+        chainId: 31337,
+        blockConfirmations: 1,
+      },
+      sepolia: {
+        chainId: 11155111,
+        blockConfirmations: 6,
+        url: ,
+        accounts: ,
+      }
+    },
+    solidity: "0.8.19",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+}
