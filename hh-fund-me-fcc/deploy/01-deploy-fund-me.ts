8514499ef5029c2ef8b536e71754e0eba19b8b0c
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-09-17 12:19:15 +0300

    Init

---
+++ hh-fund-me-fcc/deploy/01-deploy-fund-me.ts
@@ -0,0 +1,38 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+
+const DECIMALS = "8"
+const INITIAL_PRICE = "200000000000" // 2000
+
+export default async function deployMocks({
+  getNamedAccounts,
+  deployments,
+  network,
+}: HardhatRuntimeEnvironment): Promise<void> {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId
+
+  // If we are on a local development network, we need to deploy mocks!
+  if (chainId === 31337) {
+    log("Local network detected! Deploying mocks...")
+
+    await deploy("MockV3Aggregator", {
+      contract: "MockV3Aggregator",
+      from: deployer,
+      log: true,
+      args: [DECIMALS, INITIAL_PRICE],
+    })
+
+    log("Mocks Deployed!")
+    log("------------------------------------------------")
+    log(
+      "You are deploying to a local network, you'll need a local network running to interact"
+    )
+    log(
+      "Please run `npx hardhat console` to interact with the deployed smart contracts!"
+    )
+    log("------------------------------------------------")
+  }
+}
+
+deployMocks.tags = ["all", "mocks"]
