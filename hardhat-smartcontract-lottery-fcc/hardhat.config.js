9b1c66de2505c99dd9060ab8af09c86dba17b779
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-09-17 12:15:38 +0300

    Init

--- hardhat-smartcontract-lottery-fcc/hardhat.config.js
+++ hardhat-smartcontract-lottery-fcc/hardhat.config.js
@@ -6,7 +6,11 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
@@ -17,8 +21,8 @@ module.exports = {
       sepolia: {
         chainId: 11155111,
         blockConfirmations: 6,
-        url: ,
-        accounts: ,
+        url: SEPOLIA_RPC_URL,
+        accounts: [PRIVATE_KEY],
       }
     },
     solidity: "0.8.19",
